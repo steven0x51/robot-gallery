541c0803822ab12ac42a3c8f919ef0347b300217
Steven Qiang <doublestrong@hotmail.com>
2024-03-02 21:05:02 +1100

    Font and Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 import robots from './mockdata/robots.json';
 import RobotCard from './components/RobotCard';
 import styles from "./App.module.css";
@@ -7,6 +7,11 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo"/>
+        <h1>Super Awesome Robots Online Store</h1>
+          
+      </div>
       <div className={styles.robotList}>
       {robots.map( r => <RobotCard id={r.id} name={r.name} email={r.email}/>)}
       </div>
