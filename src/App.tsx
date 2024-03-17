9dd2d75e90ca1d9cbfafe7878fd26ba5cbd21e99
Steven Qiang <doublestrong@hotmail.com>
2024-03-17 16:39:05 +1100

    convert App from function to class for component life cycle

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,43 @@ import RobotCard from './components/RobotCard';
 import ShoppingCart from './components/ShoppingCart';
 import styles from "./App.module.css";
 
-function App() {
-    return (
-        <div className={styles.app}>
-            <div className={styles.appHeader}>
-                <img src={logo} className={styles.appLogo} alt="logo" />
-                <h1>Super Awesome Robots Online Store</h1>
-            </div>
+interface Props {}
 
-            <ShoppingCart />
+interface State{
+    fetchedRobotLst: any[];
+}
 
-            <div className={styles.robotList}>
-                {robots.map(r => <RobotCard id={r.id} name={r.name} email={r.email} />)}
+class App extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            fetchedRobotLst: [],
+        };
+    }
+
+    componentDidMount(): void {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(data => this.setState({fetchedRobotLst: data}));
+    }
+
+    render() {
+        return (
+            <div className={styles.app}>
+                <div className={styles.appHeader}>
+                    <img src={logo} className={styles.appLogo} alt="logo" />
+                    <h1>Super Awesome Robots Online Store</h1>
+                </div>
+
+                <ShoppingCart />
+
+                <div className={styles.robotList}>
+                    {/* {robots.map(r => <RobotCard id={r.id} name={r.name} email={r.email} />)} */}
+                    {this.state.fetchedRobotLst.map(r => <RobotCard id={r.id} name={r.name} email={r.emai}/>)}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
