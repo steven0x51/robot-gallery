74bf8ae717d6973613979cd8c07b39f42b120bd7
Steven Qiang <doublestrong@hotmail.com>
2024-04-11 20:38:36 +1000

    introduce useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './assets/images/logo.svg';
 import robots from './mockdata/robots.json';
 import RobotCard from './components/RobotCard';
@@ -6,12 +6,20 @@ import ShoppingCart from './components/ShoppingCart';
 import styles from "./App.module.css";
 
 function App() {
+
+    const [count, setCount] = useState<number>(0);
+
+    const onClickHandler = () => { setCount(count + 1) };
+    
     return (
         <div className={styles.app}>
             <div className={styles.appHeader}>
                 <img src={logo} className={styles.appLogo} alt="logo" />
                 <h1>Super Awesome Robots Online Store</h1>
             </div>
+
+            <button onClick={onClickHandler}> click </button>
+            <span> count: {count} </span>
 
             <ShoppingCart />
 
