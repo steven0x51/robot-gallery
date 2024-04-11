6e5d95cea4ec72438aa1e3940e2305364fcd1435
Steven Qiang <doublestrong@hotmail.com>
2024-04-12 00:05:54 +1000

    async, loading and error messages

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,37 @@ function App() {
 
     const [count, setCount] = useState<number>(0);
     const [remoteRobotList, setRemoteRobotList] = useState<botInfo[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>();
 
     // invokes when count changes
     useEffect(() => { document.title = `clicked ${count} times` }, [count]);
     
     // invokes when componentDidMount, only run once similar to initialise
     useEffect(() => { 
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then(response => response.json()
-        .then(data => setRemoteRobotList(data))
-        )}, []);
+        
+        
+        // promise.then() can not use try-catch, must use async to try-catch
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch("https://jsonplaceholder.typicode.com/users");
+                //.then(response => response.json()
+                //.then(data => setRemoteRobotList(data)));
+                const data = await response.json();
+                setRemoteRobotList(data);
+            }catch (e) {
+                if ( e instanceof Error ) {
+                    setError(e.message);
+                } 
+            }
+            setLoading(false);
+        }
+
+        fetchData();
+        // fetchData() is asnyc, loading will be set to false immediately while awaiting the data to be fetched.
+        //setLoading(false);
+        }, []);
 
     // invokes everytime the page is updated: without second parameter
     // infinity loop: fetched data updates the remoteRobotList -> remotRobotList cause the page to rerender -> page rerender casuse this useEffect gets called again...
@@ -49,13 +70,22 @@ function App() {
 
             <ShoppingCart />
 
-            <div className={styles.robotList}>
-                  {/* mock data from robots.json */}
-                {robots.map(r => <RobotCard id={r.id} name={r.name} email={r.email} />)}
+            <div> error message: { (error || error === "") ? <span>{error}</span> : <span>none</span> } </div>
+            {(error || error === "") && <div> Robots list error: {error} </div>}
 
-                {/* from API */}
-                {remoteRobotList.map(r => <RobotCard id={r.id} name={r.name} email={r.email} />)}
-            </div>
+            { loading ? 
+                (<h1> loading ... </h1>)
+                :
+                (<div className={styles.robotList}>
+                    
+                    {/* mock data from robots.json */}
+                    {robots.map(r => <RobotCard id={r.id} name={r.name} email={r.email} />)}
+
+                    {/* from API */}
+                    {remoteRobotList.map(r => <RobotCard id={r.id} name={r.name} email={r.email} />)}
+                    
+                </div>)
+            } 
         </div>
     );
 }
